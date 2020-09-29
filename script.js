867c7406b7a7f51c6416f27a1e186804c25414df
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-28 20:12:17 -0700

    added Java script

---
+++ script.js
@@ -0,0 +1,36 @@
+// javaScript Code written here! 
+console.log('Hello World')
+
+
+
+
+const inputBill = document.querySelector('#input-bill')
+const inputTip = document.querySelector('#input-tip')
+const inputPeople = document.querySelector('#input-people')
+const splitTip = document.querySelector('#split-tip')
+const splitBill = document.querySelector('#split-bill')
+//----------------------------------------------------------------------------
+//Calculate tip
+
+
+function calculateTip() {
+    const tipAmount = (inputBill.value  * inputTip.value / 100)/inputPeople.value
+    console.log(tipAmount, "tipAmount")    
+    splitTip.innerHTML = tipAmount.toFixed(2)
+   
+}
+calculateTip()
+//-----------------------------------------------------------------------------
+inputBill.addEventListener("click", calculateTip)
+inputTip.addEventListener('click', calculateTip)
+inputPeople.addEventListener ('click', calculateTip)
+//--------------------------------------------------------------
+
+//Calculate total bill split
+function calculateBillSplit() {
+    const guestSplit = calculateTip() + (inputBill.value/inputPeople.value)
+    console.log(guestSplit)
+
+    splitBill.innerHTML = guestSplit.toFixed(2)
+}
+calculateBillSplit()
